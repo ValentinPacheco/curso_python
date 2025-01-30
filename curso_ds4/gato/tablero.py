64f1b1c67d12190a83b1c704bb910cafbcd0326d
ValentinPacheco <valepachecoolivas@gmail.com>
2025-01-30 14:48:33 -0700

    defini juego y checa winner

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -7,11 +7,11 @@ import random
 def dibuja_tablero(simbolos_dict):
     '''dibuja el tablero del juego del gato'''
     print(f'''
-    {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
+    {simbolos_dict['1']} | {simbolos_dict['2']} | {simbolos_dict['3']}
     ---------
-    {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
+    {simbolos_dict['4']} | {simbolos_dict['5']} | {simbolos_dict['6']}
     ---------
-    {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
+    {simbolos_dict['7']} | {simbolos_dict['8']} | {simbolos_dict['9']}
     ''')
     def ia (simbolos:dict):
         '''Estrategia de la computadora'''
@@ -34,7 +34,24 @@ def dibuja_tablero(simbolos_dict):
 
                 else:
                     print('Esta casilla esta ocupada')
-
+    def juego(simbolos:dict):
+        '''juego del gato'''
+        lista_combinaciones =[
+            ['1','2','3'],
+            ['4','5','6'],
+            ['7','8','9'],
+            ['1','4','7'],
+            ['2','5','8'],
+            ['3','6','9'],
+            ['1','5','9'],
+            ['3','5','7']
+        ]
+    def checa_winner(simbolos:dict,combianciones:list):
+        '''Checa si hay un ganador'''
+        for combinacion in combianciones:
+            if simbolos[combinacion[0]] == simbolos[combinacion[1]] == simbolos[combinacion[2]]:
+                return simbolos[combinacion[0]]
+        return False
     if __name__ == '__main__':
         numeros = [str(i) for i in range(1,10)]
         dsimbolos = {x:x for x in numeros}
